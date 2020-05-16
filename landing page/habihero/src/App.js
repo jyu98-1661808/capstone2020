6433a085c549cb1be5421aec821fcf338a7588b5
jyu98-1661808 <jinayu@Jinas-MacBook-Pro.local>
2020-05-15 17:27:24 -0700

    Updated Landing Page

--- landing page/habihero/src/App.js
+++ landing page/habihero/src/App.js
@@ -13,28 +13,22 @@ function App() {
   return (
     <div className='App'>
       <header className='header'> 
-        <img src='./img/logo.png' alt='habi hero logo' />
-        <a href='https://github.com/jyu98-1661808/capstone2020'>Sign Up</a>
-        <button type='button' className='btn btn-outline-success'>Sign In</button>
+        <img src={ require('./assets/img/branding/logo.png') } alt='habi hero logo' />
+        {/* <a href='https://github.com/jyu98-1661808/capstone2020'>Sign Up</a>
+        <button type='button' className='btn btn-outline-success'>Sign In</button> */}
       </header>
-      <div className='scroll-left'>
-        <div className='scroll-content'>
-          <p>Sign up to join our user testing now! </p> 
-          <a href='https://forms.gle/ewhBruh57tSnJjK78' className="btn btn-danger">Click Here</a>
-        </div>
-      </div>
       <div className='banner'>
         <ScrollAnimation delay={800} animateIn="fadeInLeft">
-          <img src='./img/panda.png' className='panda' alt='panda' />
+          <img src={ require('./assets/img/characters/panda.png') } className='panda' alt='panda' />
         </ScrollAnimation>
         <ScrollAnimation animateIn="slideInDown">
         <div className="logo-container"> 
-          <img src='./img/white-logo.png' className='logo' alt='white' />
-          <button type="button" class="btn btn-danger">Play Now</button>
+          <img src={ require('./assets/img/branding/white-logo.png') } className='logo' alt='white logo' />
+          <a className="btn btn-danger" href='https://github.com/jyu98-1661808/capstone2020'>Play Now</a>
         </div>
         </ScrollAnimation>
         <ScrollAnimation delay={800} animateIn="fadeInRight">
-          <img src='./img/myhero.png' className='hero' alt='habi hero mascot' />
+          <img src={ require('./assets/img/characters/myhero.png') } className='hero' alt='habi hero mascot' />
         </ScrollAnimation>
       </div>
       <div className='slogan'> 
@@ -61,7 +55,7 @@ function App() {
               the environmental issues that affect our beloved animals.
             </p>
           </div>
-          <img src='./img/homepage.png' alt='habi hero homepage' />
+          <img src={ require('./assets/img/pages/game/game-main.png') } alt='habi hero game main page' />
         </div>
       </div>
       <div className='features'>
@@ -70,7 +64,7 @@ function App() {
           <ScrollAnimation delay={100} animateIn="bounce" initiallyVisible='true'>
             <div className='features-section' id="key-1">
               <h2>Create Impact</h2>
-              <img src='./img/polar-bear.png' alt='polar bear' />
+              <img src={ require('./assets/img/characters/polar-bear.png') } alt='polar bear' />
               <p>
                 Encounter a variety of endangered animals. 
                 Explore their habitat &amp; save them from threats. 
@@ -80,7 +74,7 @@ function App() {
           <ScrollAnimation delay={800} animateIn="bounce" initiallyVisible='true'>
             <div className='features-section' id="key-2">
               <h2>Be A Hero</h2>
-              <img src='./img/tiger.png' alt='tiger' />
+              <img src={ require('./assets/img/characters/tiger.png') } alt='tiger' />
               <p>
                 Solve math problems that relate to the environment. 
                 Correct answers earn coins to customize your hero!
@@ -90,7 +84,7 @@ function App() {
           <ScrollAnimation delay={1500} animateIn="bounce" initiallyVisible='true'>
             <div className='features-section' id="key-3">
               <h2>Customize Lessons</h2>
-              <img src='./img/orca.png' alt='orca' />
+              <img src={ require('./assets/img/characters/orca.png') } alt='orca' />
               <p>
                 Teachers and parents can choose one of our many lessons to customize. 
                 For each topic, our levels have filters and parameters for personalization.
@@ -106,59 +100,71 @@ function App() {
         <div className='progress-container'>
           <Carousel id='carousel'>
             <Carousel.Item>
-              <img id='homepage' src='./img/homepage.png' alt='home page' />
+              <img src={ require('./assets/img/pages/game/game-main.png') } alt='game main page' />
               <Carousel.Caption>
-                <h3>Game Homepage</h3>
+                <h3>Habi Mainpage</h3>
                 <p>Unlock new animals and earn more Habi-coins.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img src='./img/classes.png' alt='class page' />
+              <img src={ require('./assets/img/pages/game/game-environment.png') } alt='game environment' />
+              <Carousel.Caption>
+                <h3>Habi Environment</h3>
+                <p>Eliminate threats and save the habi-animals.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={ require('./assets/img/pages/game/game-modal.png') } alt='game math modal' />
+              <Carousel.Caption>
+                <h3>Habi Challenges</h3>
+                <p>Solve math problems correctly to earn habi-coins.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={ require('./assets/img/pages/game/game-shop.png') } alt='game shop' />
+              <Carousel.Caption>
+                <h3>Habi Shop</h3>
+                <p>Redeem your habi-coins and customize your hero!</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={ require('./assets/img/pages/teacher/teacher-class.png') } alt='class page' />
               <Carousel.Caption>
                 <h3>Classroom Page</h3>
                 <p>With secure class codes, manage your students and check their progress.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img src='./img/lessons.png' alt='lessons page' />
+              <img src={ require('./assets/img/pages/teacher/teacher-lessons.png') } alt='lessons page' />
               <Carousel.Caption>
                 <h3>Lessons Page</h3>
                 <p>Easily keep track of your lessons and personalize them to your liking.</p>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
-              <img src='./img/account.png' alt='account page' />
-              <Carousel.Caption>
-                <h3>Account Page</h3>
-                <p>Customize and secure your account settings.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
           </Carousel>
         </div>
       </div>
+      <div className='demo'>
+        <h1>Demo</h1>
+        <div className='demo-container'>
+          <iframe className="demo-video" src="https://www.youtube.com/embed/SnWm_Rk39J4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        </div>
+      </div>
       <div className='research'>
-        <h1>User Research</h1>
+        <h1>Notice</h1>
         <div className='research-container'>
-          <div class='video-mask'>
-            <video id='vid' loop autoPlay muted>
-              <source type="video/mp4" src="./img/hero.mp4" />
-            </video>
-          </div>
           <div className='research-description'>
-            <h3>User Research: <span style={{color: 'rgb(88, 88, 88)'}}>Covid-19 Impact</span></h3>
+            <h3>A Message for Our Habi Heroes:</h3>
             <p>
-              Due to the impact of the coronavirus pandemic on the education systems, we have pivoted our project to offer teachers &amp;
-              parents the necessary capabilities to customize the gameplay to better fit their students' academic needs.
-              We want your ideas and values met. Sign up for a short 20 minute user test or leave us an email with any suggstions.
+              Collected data by any user on the site will be deleted by June 10, 2020. 
+              You will be notified June 7th, 2020 about the deletion via email.
             </p>
-            <h3>Give Us Feedback</h3>
-            <ol>
-              <li><span style={boldStyle}>Sign up</span> for a user interview.</li>
-              <li><span style={boldStyle}>Contact us</span> via email.</li>
-              <li><span style={boldStyle}>Share</span> the page with other parents &amp; teachers.</li>
-            </ol>
+            <p>
+              HabiHero is currently deployed on Digital Ocean. 
+              On June 10, 2020 all Digital Ocean Droplets that have running Docker Containers related to HabiHero will be destroyed.
+              Docker Images created for HabiHero will be removed from Docker Hub. 
+            </p>
           </div>
-          <a href='https://forms.gle/ewhBruh57tSnJjK78' className='btn btn-success'>Sign Up</a>
         </div>
       </div>
       <div className='team'>
@@ -169,7 +175,7 @@ function App() {
               <div id='team-1' className='team-card'>
                 <h3>Jiyun Yu</h3>
                 <div className='team-mask'>
-                  <img src='./img/faces/jiyun.jpeg' alt='Jiyun Yu' />
+                  <img src={ require('./assets/img/faces/jiyun.jpeg') } alt='Jiyun Yu' />
                 </div>
                 <p className='team-job'>Front-end Developer</p>
                 <p className='team-email'>jyu98@uw.edu</p>
@@ -179,7 +185,7 @@ function App() {
               <div id='team-2' className='team-card'>
                 <h3>Andre Magallanes</h3>
                 <div className='team-mask'>
-                  <img src='./img/faces/andre.png' alt='Andre Magallanes' />
+                  <img src={ require('./assets/img/faces/andre.png') } alt='Andre Magallanes' />
                 </div>
                 <p className='team-job'>UX/UI Designer</p>
                 <p className='team-email'>magalj@uw.edu</p>
@@ -189,7 +195,7 @@ function App() {
               <div id='team-3' className='team-card'>
                 <h3>Chad Ohta</h3>
                 <div className='team-mask'>
-                  <img src='./img/faces/chad.png' alt='Chad Ohta' />
+                  <img src={ require('./assets/img/faces/chad.png') } alt='Chad Ohta' />
                 </div>
                 <p className='team-job'>Back-end Developer</p>
                 <p className='team-email'>chadohta@gmail.com</p>
@@ -199,7 +205,7 @@ function App() {
               <div id='team-4' className='team-card'>
                 <h3>Adam Pumputis</h3>
                 <div className='team-mask'>
-                  <img src='./img/faces/adam.png' alt='Adam Pumputis' />
+                  <img src={ require('./assets/img/faces/adam.png') } alt='Adam Pumputis' />
                 </div>
                 <p className='team-job'>Graphic Designer</p>
                 <p className='team-email'>akyp@uw.edu</p>
